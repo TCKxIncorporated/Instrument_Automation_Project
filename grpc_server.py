99a30bb621062ce29b6118d62acbfbc4d5aa2746
TCKxIncorporated <mr.nayif505@gmail.com>
2025-07-18 09:24:13 +0800

    more

--- grpc_server.py
+++ grpc_server.py
@@ -91,7 +91,7 @@ class InstrumentServiceServicer(instrument_pb2_grpc.InstrumentServiceServicer):
     def MonitorVoltage(self, request, context):
         reading = monitor.get_plot_data(request.channel)
         return instrument_pb2.VoltageReading(
-            time=reading["time"],
+            timestamp=reading["time"],
             voltage=reading["voltage"],
             channel=reading["channel"]
         )
